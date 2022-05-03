6f908742eef290e7594036c8361f534862261263
Sebastian Mora <chebimora02@gmail.com>
2022-05-02 22:24:52 -0500

    Implementation of style with TailWind

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -42,30 +42,40 @@ class LogIn extends React.Component{
 
     render(){
         return(
-            <Popup trigger={<button className="logInButton"> Log In </button>} modal>
-                <div className="logInContainer">
-                    <h1>Log In</h1>
-                    <form className="logInForm" onSubmit={this.handleSubmit}>
-                        <label className="logInLabel">
+            <Popup trigger={<button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"> Log In </button>} modal>
+                <div className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+                    <section>
+                    <h2 className="font-bold text-2xl">Log In</h2>
+                    <p className="text-gray-300 pt-2">Log in to your account</p>
+                    </section>
+
+                    <section>
+                    <form className="flex flex-col" onSubmit={this.handleSubmit}>
+                        <div className="mb-3 pt-3 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Usename: 
                             <input type="text" 
                                value={this.state.name} 
                                onChange={this.handleName} 
-                               className="logInputBox"
+                               className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-2 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                                placeholder='CuteCat22'
                             />
                         </label>
-                        <label className="logInLabel">
+                        </div>
+                        <div className="mb-2 pt-1 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Password: 
                             <input type="password" 
                                value={this.state.password} 
                                onChange={this.handlePswd} 
-                               className="logInputBox"
+                               className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-2 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                                placeholder='Difficut123'
                             />
                         </label>
-                        <input type="submit" value="Submit" />
+                        </div>
+                        <input className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" value="Submit" />
                     </form>
+                    </section>
                 </div>
             </Popup>
         );
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -55,56 +55,72 @@ class SignUp extends React.Component{
     render(){
         return(
             <Popup trigger={<button className="signUpButton"> Sign Up </button>} modal>
-                <div className="signUpContainer">
-                    <h1>Sign Up</h1>
-                    <form className="signUpForm" onSubmit={this.handleSubmit}>
-                        <label className="signUpLabel">
+                <div className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+                    <section>
+                    <h2 className="font-bold text-2xl">Sign Up</h2>
+                    <p className="text-gray-300 pt-2">Please create account to join the club!</p>
+                    </section>
+
+                    <section className="mt-2">
+                    <form className="flex flex-col" onSubmit={this.handleSubmit}>
+                        <div className="mb-2 pt-1 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Username:
                             <input type="text" 
                                value={this.state.name} 
                                onChange={this.handleName} 
-                               className="signInputBox"
+                               className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-2 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                                placeholder='CuteCat22'
                             />
                         </label>
-                        <label className="signUpLabel">
+                        </div>
+                        <div className="mb-2 pt-1 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Birthday:
                             <input 
                                 type="date" 
                                 value={this.state.birthday} 
                                 onChange={this.handleBirthDay} 
-                                className="signInputBox"
+                                className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                             />
                         </label>
-                        <label className="signUpLabel">
+                        </div>
+                        <div className="mb-2 pt-1 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Country:
                             <input type="country" 
                                value={this.state.country} 
                                onChange={this.handleCountry} 
-                               className="signInputBox"
+                               className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                                placeholder='Wonderland'
                             />
                         </label>
-                        <label className="signUpLabel">
+                        </div>
+                        <div className="mb-2 pt-1 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Password:
                             <input type="password" 
                                value={this.state.password} 
                                onChange={this.handlePswd} 
-                               className="signInputBox"
+                               className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                                placeholder='Difficult123'
                             />
                         </label>
-                        <label className="signUpLabel">
+                        </div>
+                        <div className="mb-2 pt-1 rounded bg-gray-200">
+                        <label className="block text-gray-700 text-sm font-bold mb-2 ml-3">
                             Mail:
                             <input type="mail" 
                                value={this.state.mail} 
                                onChange={this.handleMail} 
-                               className="signInputBox"
+                               className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
                                placeholder='Katsby@miau.com'
                             />
                         </label>
-                        <input type="submit" value="Submit" />
+                        </div>
+                        <input className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" value="Submit" />
                     </form>
+                    </section>
                 </div>
             </Popup>
         );
